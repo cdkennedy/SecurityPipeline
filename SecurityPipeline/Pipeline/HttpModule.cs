d76771e6e496d770ca536945f80f95f8afe72c6b
piotrekbass <piotrek.koscielniak@gmail.com>
2014-06-02 23:55:30 +0200

    The different options of authentication: Httpmodule, Middleware, AuthenticateFilter, AuthorizeFilter.

---
+++ SecurityPipeline/Pipeline/HttpModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SecurityPipeline.Pipeline
+{
+	//TODO usniete została referencja do System.Web
+	//public class HttpModule : IHttpModule
+	//{
+
+	//	public void Dispose()
+	//	{
+	//	}
+
+	//	public void Init(HttpApplication context)
+	//	{
+	//		context.BeginRequest += context_BeginRequest;
+	//	}
+
+	//	void context_BeginRequest(object sender, EventArgs e)
+	//	{
+	//		Helper.Write("HttpModule", HttpContext.Current.User);
+	//	}
+	//}
+}
---
+++ SecurityPipeline/Pipeline/TestMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SecurityPipeline.Pipeline
+{
+	public class TestMiddleware
+	{
+		public TestMiddleware(Func<IDictionary<string, object>, Task> next)
+		{
+			_next = next;
+		}
+		 
+		public async Task Invoke(IDictionary<string, object> env) 
+		{
+			var context = new OwinContext(env);
+
+			Helper.Write("Middleware", context.Request.User);
+
+			context.Request.User = new GenericPrincipal(new GenericIdentity("dom"), new string[] { });
+
+			await _next(env);
+		}
+
+		public Func<IDictionary<string, object>, Task> _next { get; set; }
+	}
+}
